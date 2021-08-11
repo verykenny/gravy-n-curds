531d733750cc91f4dba467d21b52a474d9f4e67c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-11 13:56:25 -0600

    implemented sign up validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,13 +3,15 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { validateSignup } = require('../../utils/validation');
+
 
 
 const router = express.Router();
 
 
 // Sign up
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', validateSignup, asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
 
