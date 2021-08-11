a891dbb5d9f453f50743fbcaffba74cf3721ff9b
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-11 16:53:06 -0600

    added font awesome

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -31,7 +31,8 @@ function ProfileButton({ user }) {
     return (
         <>
             <button onClick={openMenu}>
-                <i className="fas fa-user-circle" />
+                <i className="fas fa-user" />
+                Account
             </button>
             {showMenu && (
                 <ul className="profile-dropdown">
