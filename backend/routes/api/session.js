53024edd0b4637dca6d7d1621bb5dd2f62e67b18
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-11 13:46:13 -0600

    implemented route to get user session information

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,21 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Restore user session
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
+
 
 // log in
 router.post('/', asyncHandler(async (req, res, next) => {
