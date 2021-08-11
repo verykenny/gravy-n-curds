32aee54d612fadf549d8ddddeecd476bcb56816a
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-10 19:26:31 -0600

    added backend api test route

---
+++ backend/routes/api/index.js
@@ -0,0 +1,9 @@
+const app = require('../../app');
+
+const router = require('express').Router();
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+module.exports = router;
