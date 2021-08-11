d05d7c62b651dab6d457de34c9d8aa3ca01bd617
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-10 19:54:44 -0600

    added user auth middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,15 @@
 const app = require('../../app');
 
+
+const { User } = require('../../db/models');
 const router = require('express').Router();
+
+
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
+
+
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,4 +11,10 @@ router.get('/hello/world', (req, res) => {
     res.send('Hello World!');
 });
 
+
+
+
+
+
+
 module.exports = router;
