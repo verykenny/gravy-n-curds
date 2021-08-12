9761e3d04a505c6e29f52743aaae0ab9a6e207cb
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-12 13:33:55 -0600

    added error handling for login

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,54 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Redirect, useHistory } from 'react-router-dom';
 import { logInUser } from '../../store/session';
 
-import './LoginForm.css'
+import './LoginForm.css';
 
 function LoginForm({ setShowModal }) {
     const [credential, setCredential] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
     const dispatch = useDispatch();
     const history = useHistory();
     const sessionUser = useSelector((state) => state.session.user);
 
-    if (sessionUser) return <Redirect to="/"></Redirect>;
-
     const handleSubmit = async (e) => {
+        setErrors([]);
         e.preventDefault();
         try {
             await dispatch(logInUser(credential, password));
             history.go(-1);
         } catch (e) {
-            // const error = await e.json();
-            console.log('error that was returned', e);
-            // TODO:
-            // 1. Handle and display these errors
+            const res = await e.json();
+            const { errors } = res
+            setErrors(errors);
         }
     };
 
+    if (sessionUser) return <Redirect to="/"></Redirect>;
+
     return (
-        <div className='form-container login-form'>
+        <div className="form-container login-form">
+            <div className='errors-container'>
+            {errors.includes('The provided credentials were invalid.') && (
+                        <p className='form-custom-error'>The provided credentials were invalid.</p>
+                    )}
+            </div>
             <form onSubmit={handleSubmit}>
-                <div className='widget-container'>
+                <div className="widget-container">
                     <input
-                        type='text'
-                        placeholder='username or email'
+                        type="text"
+                        placeholder="username or email"
                         value={credential}
                         onChange={(e) => setCredential(e.target.value)}
                     ></input>
+                    {errors.includes('Please provide a valid email or username.') && (
+                        <p className='form-custom-error'>Please provide a valid email or username</p>
+                    )}
                 </div>
-                <div className='widget-container'>
+                <div className="widget-container">
                     <input
-                        type='password'
-                        placeholder='password'
+                        type="password"
+                        placeholder="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     ></input>
+                    {errors.includes('Please provide a password.') && (
+                        <p className='form-custom-error'>Please provide a password</p>
+                    )}
                 </div>
-                <div className='btn-container'>
-                    <button className='btn btn-primary' type="submit">Log In</button>
-                    <Link className='btn btn-alt' to="/signup" onClick={() => setShowModal(false)}>Sign Up</Link>
+                <div className="btn-container">
+                    <button className="btn btn-primary" type="submit">
+                        Log In
+                    </button>
+                    <Link
+                        className="btn btn-alt"
+                        to="/signup"
+                        onClick={() => setShowModal(false)}
+                    >
+                        Sign Up
+                    </Link>
                 </div>
             </form>
         </div>
