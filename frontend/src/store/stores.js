f9f1e037c24d3f2d2246e59badefb3b1bdeb5aac
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 16:03:27 -0600

    implemented create and remove Store thunk actions

--- frontend/src/store/stores.js
+++ frontend/src/store/stores.js
@@ -4,46 +4,63 @@ import { csrfFetch } from "./csrf";
 
 
 const GET_STORES = 'stores/setStores';
-const UPDATE_STORE = 'stores/updateStore';
+const ADD_STORE = 'stores/addStore';
 const DELETE_STORE = 'stores/deleteStore';
 
 const setStores = (stores) => ({
     type: GET_STORES,
     payload: stores
-})
+});
 
-const updateStore = (store) => ({
-    type: UPDATE_STORE,
+const addStore = (store) => ({
+    type: ADD_STORE,
     payload: store
-})
+});
 
-const deleteStore = (store) => ({
+const deleteStore = (storeId) => ({
     type: DELETE_STORE,
-    payload: store
-})
+    payload: storeId
+});
 
 export const getStores = () => async dispatch => {
     const res = await csrfFetch('/api/stores');
-
     const data = await res.json();
-    dispatch(setStores(data.stores))
+    dispatch(setStores(data.stores));
+};
+
+export const createStore = (name, imageUrl) => async dispatch => {
+    const res = await csrfFetch('/api/stores', {
+        method: 'POST',
+        body: JSON.stringify({ name, imageUrl })
+    })
+    const data = await res.json();
+    dispatch(addStore(data.store));
 };
 
 
+export const removeStore = (storeId) => async dispatch => {
+    await csrfFetch(`/api/stores/${storeId}`);
+    dispatch(deleteStore(storeId));
+}
 
 
-const initialState = { stores: null };
+
+const initialState = {};
 
 
 const storesReducer = (state = initialState, action) => {
-
+    Object.freeze(state);
+    let newState = { ...state };
     switch (action.type) {
         case GET_STORES:
-            const newState = { ...action.payload };
+            return newState;
+        case ADD_STORE:
+            return newState;
+        case DELETE_STORE:
             return newState;
         default:
             return state;
     }
-}
+};
 
 export default storesReducer;
