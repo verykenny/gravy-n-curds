b0661ff98269cd96aa20c8e7ecb60972ed5432df
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 11:18:12 -0600

    added and tested functionality for store routes

--- backend/routes/api/stores.js
+++ backend/routes/api/stores.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 
 const { Store } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 
 
 // GET retrieve all stores
@@ -12,23 +13,48 @@ router.get('/', asyncHandler(async (req, res) => {
 
 
 // POST add a new store
-router.post('/', asyncHandler(async (req, res) => {
-    const { ownerId, name, imageUrl } = req.body;
-    return res.json({ message: 'good to go' });
+router.post('/', requireAuth, asyncHandler(async (req, res) => {
+    const { name, imageUrl } = req.body;
+    const ownerId = req.user.id;
+
+    const store = await Store.create({
+        ownerId,
+        name,
+        imageUrl,
+    });
+
+    return res.json({ store });
 }))
 
 
 // PUT update a store
-router.put('/:storeId(\\d+)', asyncHandler(async (req, res) => {
-    const storeId  = Number(req.params.storeId)
-    return res.json({ message: storeId });
+router.put('/:storeId(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+    const { name, imageUrl } = req.body;
+    const storeId  = Number(req.params.storeId);
+
+    const userId = req.user.id;
+
+
+    const store = await Store.findByPk(storeId);
+
+    if (userId !== store.ownerId) return res.json({ message: 'unauthorized' })
+
+    store.name = name;
+    store.imageUrl = imageUrl;
+    await store.save();
+
+    return res.json({ store });
 }))
 
 
 // DELETE remove a store
-router.delete('/:storeId(\\d+)', asyncHandler(async (req, res) => {
-    const storeId  = Number(req.params.storeId)
-    return res.json({ message: storeId });
+router.delete('/:storeId(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+    const storeId  = Number(req.params.storeId);
+
+    const store = await Store.findByPk(storeId);
+    await store.destroy();
+
+    return res.json({ message: 'success' });
 }))
 
 
