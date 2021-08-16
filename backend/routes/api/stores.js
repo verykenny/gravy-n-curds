2cef294e4d8e643063b734b39001a403d19fe371
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 10:59:45 -0600

    added test routes for stores

--- backend/routes/api/stores.js
+++ backend/routes/api/stores.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const { Store } = require('../../db/models');
 
 
+// GET retrieve all stores
+router.get('/', asyncHandler(async (req, res) => {
+    const stores = await Store.findAll();
+    return res.json({ stores });
+}))
 
+
+// POST add a new store
+router.post('/', asyncHandler(async (req, res) => {
+    const { ownerId, name, imageUrl } = req.body;
+    return res.json({ message: 'good to go' });
+}))
+
+
+// PUT update a store
+router.put('/:storeId(\\d+)', asyncHandler(async (req, res) => {
+    const storeId  = Number(req.params.storeId)
+    return res.json({ message: storeId });
+}))
+
+
+// DELETE remove a store
+router.delete('/:storeId(\\d+)', asyncHandler(async (req, res) => {
+    const storeId  = Number(req.params.storeId)
+    return res.json({ message: storeId });
+}))
 
 
 
