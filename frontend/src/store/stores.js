ce42cf5d8551afadbed2ae839881b8649fc1c5f4
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 15:24:28 -0600

    created action creators for getting updating and deleteing stores

--- frontend/src/store/stores.js
+++ frontend/src/store/stores.js
@@ -1,8 +1,23 @@
 // import { csrfFetch } from './csrf';
 
 
+const GET_STORES = 'stores/getStores';
+const UPDATE_STORE = 'stores/updateStore';
+const DELETE_STORE = 'stores/deleteStore';
 
+const getStores = () => ({
+    type: GET_STORES,
+})
 
+const updateStore = (store) => ({
+    type: UPDATE_STORE,
+    payload: store
+})
+
+const deleteStore = (store) => ({
+    type: DELETE_STORE,
+    payload: store
+})
 
 
 
