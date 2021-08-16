1dd9d32479b7b1e47379d0a4a3c4b9f0230eace9
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 16:42:01 -0600

    created action creators for poutine

--- frontend/src/store/poutine.js
+++ frontend/src/store/poutine.js
@@ -1,5 +1,32 @@
 import { csrfFetch } from "./csrf";
 
+const SET_POUTINES = 'poutines/setPoutines';
+const ADD_POUTINE = 'poutines/addPoutine';
+const DELETE_POUTINE = 'poutines/deletePoutine'
+
+const setPoutines = (poutines) => ({
+    type: SET_POUTINES,
+    payload: poutines
+});
+
+const addPoutine = (poutine) => ({
+    type: ADD_POUTINE,
+    payload: poutine
+});
+
+const deletePoutine = (poutineId) => ({
+    type: DELETE_POUTINE,
+    payload: poutineId
+});
+
+export const getPoutines = () => async dispatch => {
+    const res = await csrfFetch('/api/poutines');
+    const data = await res.json();
+    dispatch(setPoutines(data.poutines));
+};
+
+
+
 
 
 
