205aef1bb1f9907313c822821c05d4c697ed878c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 10:47:29 -0600

    added router file for stores with import to the api root file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const storeRouter = require('./stores');
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
+router.use('/stores', storeRouter);
 
 
 module.exports = router;
