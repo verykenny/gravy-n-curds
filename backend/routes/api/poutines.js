2d7dc5dcc91487aa0c38692acb43c5720496c8f3
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-16 11:29:12 -0600

    created template routes for poutine router

--- backend/routes/api/poutines.js
+++ backend/routes/api/poutines.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+// GET retrieve all poutines
+router.get('/', asyncHandler(async (req, res) => {
+
+    res.json({ message: 'success' })
+}))
 
 
+// POST add new poutine
+router.post('/', asyncHandler(async (req, res) => {
+
+    res.json({ message: 'success' })
+}))
+
+
+// PUT update a poutine
+router.put('/:poutineId(\\d+)', asyncHandler(async (req, res) => {
+
+    res.json({ message: 'success' })
+}))
+
+
+// DELETE remove a poutine
+router.delete('/:poutineId(\\d+)', asyncHandler(async (req, res) => {
+
+    res.json({ message: 'success' })
+}))
 
 
 module.exports = router;
