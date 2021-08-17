050ced67fcc6f33c655c07ca6b73e8528c32353a
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-17 10:33:31 -0600

    added logout button to nav bar and profile link

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,17 +2,21 @@ import { NavLink } from "react-router-dom";
 
 
 import './Navigation.css'
-import ProfileButton from "./ProfileButton";
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import LoginFormModal from "../LoginFormModal";
+import { logOutUser } from "../../store/session";
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user)
+    const dispatch = useDispatch();
 
     let sessionLinks;
     if (sessionUser) {
         sessionLinks = (
-            <ProfileButton user={sessionUser} />
+            <>
+                <NavLink to='/profile'>Profile</NavLink>
+                <button onClick={() => dispatch(logOutUser())}>Log Out</button>
+            </>
         );
     } else {
         sessionLinks = (
@@ -24,15 +28,15 @@ function Navigation({ isLoaded }) {
     }
 
     return (
-            <nav className='nav-container'>
-                <p>gravy-n-curds</p>
-                <ul>
-                    <li>
-                        <NavLink exact to="/">Home</NavLink>
-                        {isLoaded && sessionLinks}
-                    </li>
-                </ul>
-            </nav>
+        <nav className='nav-container'>
+            <p>gravy-n-curds</p>
+            <ul>
+                <li>
+                    <NavLink exact to="/">Home</NavLink>
+                    {isLoaded && sessionLinks}
+                </li>
+            </ul>
+        </nav>
     )
 }
 
