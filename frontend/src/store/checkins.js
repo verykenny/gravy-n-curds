ce7722868e7b656f8ab224ea884948ec5fabde80
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-17 09:42:54 -0600

    refactor checkin reducer for public and private state, updated getCheckins thunk action to make call for data for each state

--- frontend/src/store/checkins.js
+++ frontend/src/store/checkins.js
@@ -4,9 +4,12 @@ const SET_CHECKINS = 'checkins/setCheckins';
 const ADD_CHECKIN = 'checkins/addCheckin';
 const DELETE_CHECKIN = 'checkins/deleteCheckin';
 
-const setCheckins = (checkins) => ({
+const setCheckins = (publicCheckins, privateCheckins) => ({
     type: SET_CHECKINS,
-    payload: checkins,
+    payload: {
+        public: publicCheckins,
+        private: privateCheckins
+    },
 });
 
 const addCheckin = (checkin) => ({
@@ -20,9 +23,11 @@ const deleteCheckin = (checkinId) => ({
 });
 
 export const getCheckins = () => async (dispatch) => {
-    const res = await csrfFetch('/api/checkins');
-    const data = await res.json();
-    dispatch(setCheckins(data.checkins));
+    const resPublic = await csrfFetch('/api/checkins');
+    const resPrivate = await csrfFetch('/api/users/checkins');
+    const dataPublic = await resPublic.json();
+    const dataPrivate = await resPrivate.json();
+    dispatch(setCheckins(dataPublic.checkins, dataPrivate.checkins));
 };
 
 export const createCheckin =
@@ -52,22 +57,27 @@ export const removeCheckin = (checkinId) => async (dispatch) => {
     dispatch(deleteCheckin(checkinId));
 };
 
-const initialState = {};
+const initialState = { public: {}, private: {} };
 
 const checkinsReducer = (state = initialState, action) => {
     Object.freeze(state);
     let newState = { ...state };
+    newState.public = { ...state.public };
+    newState.private = { ...state.private };
     switch (action.type) {
         case SET_CHECKINS:
-            action.payload.forEach((checkin) => {
-                newState[checkin.id] = checkin;
+            action.payload.public.forEach((checkin) => {
+                newState.public[checkin.id] = checkin;
             });
+            action.payload.private.forEach((checkin) => {
+                newState.private[checkin.id] = checkin
+            })
             return newState;
         case ADD_CHECKIN:
-            newState[action.payload.id] = action.payload;
+            newState.private[action.payload.id] = action.payload;
             return newState;
         case DELETE_CHECKIN:
-            delete newState[action.payload];
+            delete newState.private[action.payload];
             return newState;
         default:
             return state;
