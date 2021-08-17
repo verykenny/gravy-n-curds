a3528d83acf00d17b7f8d4a3b490428f840fd27a
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-17 08:54:49 -0600

    implemented thunk action and switch case for setting checkins

--- frontend/src/store/checkins.js
+++ frontend/src/store/checkins.js
@@ -1,9 +1,32 @@
+import { csrfFetch } from "./csrf";
+
+const SET_CHECKINS = 'checkins/setCheckins';
+
+const setCheckins = (checkins) => ({
+    type: SET_CHECKINS,
+    payload: checkins
+});
+
+
+export const getCheckins = () => async dispatch => {
+    const res = await csrfFetch('/api/checkins');
+    const data = await res.json();
+    dispatch(setCheckins(data.checkins));
+
+
+}
+
 const initialState = {};
 
 const checkinsReducer = (state = initialState, action) => {
     Object.freeze(state);
-    // let newState = { ...state };
+    let newState = { ...state };
     switch (action.type) {
+        case SET_CHECKINS:
+            action.payload.forEach(checkin => {
+                newState[checkin.id] = checkin
+            })
+            return newState;
         default:
             return state;
     }
