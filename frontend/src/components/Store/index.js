b752d6e59e3bd63461d26fe66e02f2592c24a048
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-18 09:24:22 -0600

    added store page with cards for poutine

--- frontend/src/components/Store/index.js
+++ frontend/src/components/Store/index.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getStores } from "../../store/stores";
+
+import './Store.css'
 
 
 const Store = () => {
@@ -13,9 +15,53 @@ const Store = () => {
         dispatch(getStores())
     }, [dispatch])
 
+    const storeContent = () => {
+        return (
+            <>
+                <h1>{store.name}</h1>
+
+            </>
+        )
+    }
+
+    const storeImage = () => {
+        return <img src={store.imageUrl} alt={store.name} />
+    }
+
     return (
-        <h1>{store.name}</h1>
+        <>
+            <div className='store-container'>
+                <div className='store-content'>
+                    {store && storeContent()}
+                </div>
+                <div className='store-image'>
+                    {store && storeImage()}
+                </div>
+            </div>
+            <div className='poutine-list-container'>
+                {store && store.Poutines.map(poutine => (
+                    <PoutineCard poutine={poutine} />
+                ))}
+            </div>
+        </>
     )
 }
+
+
+
+const PoutineCard = ({ poutine }) => {
+    return (
+        <div className='poutine-card'>
+            <img src={poutine.imageUrl} alt={poutine.name} />
+            <div className='poutine-card-content'>
+                <p>{poutine.name}</p>
+                <p>{poutine.description}</p>
+            <Link to={`/poutines/${poutine.id}`}>more info</Link>
+            <Link to={`/poutine/${poutine.id}/checkins/create`} className='btn btn-alt'>Check-in</Link>
+            </div>
+        </div>
+    )
+}
+
 
 export default Store;
