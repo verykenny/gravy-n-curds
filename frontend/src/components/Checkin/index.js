647b84ff6917b9505625e912e32471d915f08bbd
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-18 09:42:37 -0600

    added checkin page

--- frontend/src/components/Checkin/index.js
+++ frontend/src/components/Checkin/index.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getCheckins } from "../../store/checkins";
 
+import './Checkin.css'
 
 const Checkin = () => {
     const { checkinId } = useParams();
@@ -13,9 +14,30 @@ const Checkin = () => {
         dispatch(getCheckins())
     }, [dispatch])
 
+    const checkinContent = () => {
+        return (
+            <>
+                <p>{checkin.User.username} ate <Link to={`/poutines/${checkin.Poutine.id}`}>{checkin.Poutine.name}</Link> at <Link to={`/stores/${checkin.Poutine.Store.id}`}>{checkin.Poutine.Store.name}</Link>.</p>
+                <p>{checkin.comment}</p>
+                <p>Rating: {checkin.rating}</p>
+            </>
+        )
+    }
+
+    const checkinImage = () => {
+        return <img src={checkin.Poutine.imageUrl} alt={checkin.Poutine.name} />
+    }
+
     return (
         <>
-            <h1>{checkin.User.username}</h1>
+            <div className='checkin-container'>
+                <div className='checkin-content'>
+                    {checkin && checkinContent()}
+                </div>
+                <div className='checkin-image'>
+                    {checkin && checkinImage()}
+                </div>
+            </div>
         </>
     )
 }
