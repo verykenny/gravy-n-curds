44f89f95a813142b970499aa46e740249141cad4
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 10:45:12 -0600

    added first part of home page with info and svg images

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from 'react-router-dom'
 import { getPoutines } from "../../store/poutine";
 
+import './Home.css'
 
 const Home = () => {
     const poutines = useSelector(state => state.poutines)
@@ -12,13 +14,25 @@ const Home = () => {
     }, [dispatch])
 
     return (
-        <div>
-            {Object.values(poutines) && Object.values(poutines).map(poutine => (
-                <div key={poutine.id}>
-                    <h2>{poutine.name}</h2>
-                    <img src={poutine.imageUrl} alt={poutine.name}></img>
+        <div className='main-container'>
+            <div className='splash-container' style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/images/splash-image.jpeg` }}>
+                <div className='info-container'>
+                    <div className='info-card'>
+                        <img src='/images/poutine-icon-white.svg' alt='poutine icon' />
+                        <h1>gravyNcurds</h1>
+                        <h2>EAT GOOD STUFF</h2>
+                        <p className='tag-line'>Discover and share your favorite poutine dishes.</p>
+                        <p>Get started with some <Link to='/top-rated'>top-rated dishes</Link>.</p>
+                    </div>
                 </div>
-            ))}
+                <div className='photo-container'>
+                        <img src='/images/poutine-side-image-3.png' alt='poutine icon' />
+                </div>
+            </div>
+            <div className='feature-container'>
+                <div className='example-container'></div>
+                <div className='links-container'></div>
+            </div>
         </div>
     )
 }
