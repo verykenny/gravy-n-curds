69414d9904ad0f8d6cc77a1bbec39a51d6e01813
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 15:21:57 -0600

    created tabbed selection box for top poutine and top restaurants

--- frontend/src/components/TopRated/index.js
+++ frontend/src/components/TopRated/index.js
@@ -1,14 +1,34 @@
-
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import './TopRated.css'
 
 
 const TopRated = () => {
+    const [topToggle, setTopToggle] = useState(true)
+
+    const sum = (accum, currentValue) => accum + currentValue;
+
+    const topPoutines = useSelector(state => Object.values(state.poutines).sort((a, b) => b.Checkins.length - a.Checkins.length).slice(0, 10))
+
     return (
         <>
-        <h1>Top Rated</h1>
+            <div className='top-page-container'>
+                <div className='top-menu-container'>
+                    <button className={`btn ${(topToggle) ? 'active': 'disabled'}`} onClick={() => setTopToggle(true)}><h1>Top Rated Poutine</h1></button>
+                    <button className={`btn ${(!topToggle) ? 'active': 'disabled'}`} onClick={() => setTopToggle(false)}><h1>Top Rated Stores</h1></button>
+                </div>
+                <div className='top-show-container'>
+                    {topToggle && <p>Top Poutine</p>}
+                    {!topToggle && <p>Top Store</p>}
+                </div>
+            </div>
         </>
     )
 }
+
+
+
+
 
 export default TopRated;
