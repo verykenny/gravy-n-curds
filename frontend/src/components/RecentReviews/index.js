627c636d07929e80a410f0d9b8aeef81ede0132c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 11:34:19 -0600

    created and mounted components for recent and top pages

---
+++ frontend/src/components/RecentReviews/index.js
@@ -0,0 +1,15 @@
+
+
+import './RecentReviews.css'
+
+
+const RecentReviews = () => {
+    return (
+        <>
+            <h1>Recent Reviews</h1>
+        </>
+    )
+}
+
+
+export default RecentReviews;
