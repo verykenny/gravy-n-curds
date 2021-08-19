080940d9e36fd710e1bf2ce02d276f8014d2dcbe
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 11:15:44 -0600

    added links tile to the home page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -35,7 +35,12 @@ const Home = () => {
                         <h2>check-in and rate poutine</h2>
                         <p>...with check-ins, you can keep track of what you've tried ans what you thought of it!</p>
                     </div>
-                    <div className='links-container'></div>
+                    <div className='links-container'>
+                        <h2>Getting Started</h2>
+                        <p>See <Link to='/top-rated'>top-rated</Link> poutine dishes.</p>
+                        <p>Check out some <Link to='/recent'>recent reviews/check-ins</Link> that other poutine lovers have submitted.</p>
+                        <p>Get started by <Link to='signup'>signing up</Link>!</p>
+                    </div>
                 </div>
             </div>
         </>
