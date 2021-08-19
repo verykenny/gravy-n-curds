a4c7bfb78a279937f3963929ccabc486fcf90250
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 12:15:50 -0600

    creating checkin card layout

--- frontend/src/components/RecentReviews/index.js
+++ frontend/src/components/RecentReviews/index.js
@@ -1,20 +1,49 @@
-
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './RecentReviews.css'
 
 
 const RecentReviews = () => {
+
+    const recentCheckins = useSelector(state => Object.values(state.checkins).sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)))
+
+
+
     return (
         <>
             <div className='recent-page-container'>
                 <div className='recent-reviews-container'>
+                    <h2>Recent activity</h2>
+                    {recentCheckins && recentCheckins.map(checkin => (
+                        <CheckinCard key={checkin.id} checkin={checkin} />
+                    ))}
 
                 </div>
                 <div className='trending-poutine-container'>
-
+                    <h2>Trending Dishes</h2>
                 </div>
             </div>
         </>
+    )
+}
+
+
+const CheckinCard = ({ checkin }) => {
+
+    return (
+        <div className='checkin-card'>
+            <div>
+                <img src='/images/poutine-icon-red-background.svg' alt='poutine icon' />
+                Restaurant: <Link to={`/stores/${checkin.Poutine.Store.id}`}>{checkin.Poutine.Store.name}</Link>
+                Dish: <Link to={`/poutines/${checkin.Poutine.id}`}>{checkin.Poutine.name}</Link>
+            </div>
+            <p>{checkin.User.username} ate .</p>
+            <p>Comment: </p>
+            <p>{checkin.comment}</p>
+            <p>Rating: {checkin.rating}</p>
+            <Link to={`/checkins/${checkin.id}`}>more info</Link>
+        </div>
     )
 }
 
