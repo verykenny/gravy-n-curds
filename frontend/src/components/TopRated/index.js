b436fe342714c0ac296bd8216b63b7519d5d3ab0
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-19 15:45:47 -0600

    styling the top poutine cards

--- frontend/src/components/TopRated/index.js
+++ frontend/src/components/TopRated/index.js
@@ -9,7 +9,7 @@ import './TopRated.css'
 const TopRated = () => {
     const [topToggle, setTopToggle] = useState(true)
 
-    const sum = (accum, currentValue) => accum + currentValue;
+    const average = (accum, currentValue) => accum + currentValue.rating;
 
     const topPoutines = useSelector(state => Object.values(state.poutines).sort((a, b) => b.Checkins.length - a.Checkins.length).slice(0, 10))
 
@@ -17,12 +17,12 @@ const TopRated = () => {
         <>
             <div className='top-page-container'>
                 <div className='top-menu-container'>
-                    <button className={`btn ${(topToggle) ? 'active': 'disabled'}`} onClick={() => setTopToggle(true)}><h1>Top Rated Poutine</h1></button>
-                    <button className={`btn ${(!topToggle) ? 'active': 'disabled'}`} onClick={() => setTopToggle(false)}><h1>Top Rated Stores</h1></button>
+                    <button className={`btn ${(topToggle) ? 'active' : 'disabled'}`} onClick={() => setTopToggle(true)}><h1>Top Rated Poutine</h1></button>
+                    <button className={`btn ${(!topToggle) ? 'active' : 'disabled'}`} onClick={() => setTopToggle(false)}><h1>Top Rated Stores</h1></button>
                 </div>
                 <div className='top-show-container'>
                     {(topToggle && topPoutines.length > 0) && topPoutines.map(poutine => (
-                        <TopPoutines key={poutine.id} poutine={poutine}/>
+                        <TopPoutines key={poutine.id} poutine={poutine} />
                     ))}
                     {!topToggle && <TopStores />}
                 </div>
@@ -36,15 +36,19 @@ const TopPoutines = ({ poutine }) => {
     return (
         <div className='top-poutine-card'>
             <div className='top-poutine-image'>
-                <img src={poutine.imageUrl} alt={poutine.name}/>
+                <img src={poutine.imageUrl} alt={poutine.name} />
             </div>
             <div className='top-poutine-content'>
                 <h2>{poutine.name}</h2>
                 <p>{poutine.description}</p>
+                <p>Average rating</p>
+                <p>number of checkins</p>
             </div>
             <div className='top-poutine-access'>
-                    <CheckinFormModal poutine={poutine} store={poutine.Store}/>
-                    <Link to={`/stores/${poutine.Store.id}`}>{poutine.Store.name}</Link>
+                <CheckinFormModal poutine={poutine} store={poutine.Store} />
+                <Link to={`/stores/${poutine.Store.id}`}>{poutine.Store.name}</Link>
+                <Link to={`/poutines/${poutine.id}`}>more info</Link>
+                <p>Link to poutine page</p>
             </div>
         </div>
     )
