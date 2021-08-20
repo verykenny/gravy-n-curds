cf323fc88d1173596583df24fbff06302be0b3ef
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-20 14:26:59 -0600

    added custom page not found page

---
+++ frontend/src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found-container'>
+            <img src='/images/poutine-side-image-1.png' alt='page not found poutine'/>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound;
