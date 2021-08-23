927cbfb21e65267eb17f8d09c8bfb85bfc477896
verykenny <16845292+verykenny@users.noreply.github.com>
2021-08-23 08:02:08 -0600

    added no rating note

--- frontend/src/components/Poutine/index.js
+++ frontend/src/components/Poutine/index.js
@@ -21,7 +21,7 @@ const Poutine = () => {
     const averageRating = (checkins) => {
         const totalCheckins = checkins.length;
         const sum = checkins.reduce((accum, ele) => accum + ele.rating, 0)
-        return (sum / totalCheckins).toFixed(1);
+        return (sum / totalCheckins).toFixed(1) > 0 ?  (sum / totalCheckins).toFixed(1) > 0  : 'no rating';
     }
 
     const poutineContent = () => {
